b072f864a97d38e1e3ef58f92c793cb9be1a8b9c
Unknown <912562151@qq.com>
2018-05-29 23:51:57 +0800

    把这个文章修改下

--- js/word.js
+++ js/word.js
@@ -1,34 +1,30 @@
-define([], function() {
+define([], function () {
     return WordContentJson = {
         css: `html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,font,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,textarea,input{margin:0;padding:0}address,cite,dfn,em,var,i{font-style:normal}body{font-size:16px;line-height:1.5;font-family:'Microsoft Yahei','simsun','arial','tahoma';color:#222;background:#eee}table{border-collapse:collapse;border-spacing:0}h1,h2,h3,h4,h5,h6,th{font-size:100%;font-weight:normal}button,input,select,textarea{font-size:100%}fieldset,img{border:0}a{text-decoration:none;color:#666;background:0}ul,ol{list-style:none}:focus{outline:0}.clearfix{clear:both;content:"";display:block;overflow:hidden}.clear{clear:both}.fl{float:left}.fr{float:right}`,
         javascript: "var o = {a: 1};",
         html: `<html><head><title><base><link><meta><style><script><noscript><template><body><section><nav><article><aside><h1>,<h2>,<h3>,<h4>,<h5>,<h6><header><footer><address><main><p><hr><pre><blockquote><ol><ul><li><dl><dt><dd><figure><figcaption><div><a><em><strong><small><s><cite><q><dfn><abbr><data><time><code><var><samp><kbd><sub><sup><i><b><u><mark><ruby><rt><rp><bdi><bdo><span><br><wbr><ins><del><img><iframe><embed><object><param><video><audio><source><track><canvas><map><area><svg><table><caption><colgroup><col><tbody><thead><tfoot><tr><td><th><form><fieldset><legend><label><input><button><select><datalist><optgroup><option><textarea><keygen><output><progress><meter><details><summary><menuitem><menu>`,
         english: "The past is gone and static. Nothing we can do will change it. Thefuture is before us and dynamic. Everything we do will affect it. " +
-            "You laugh at me for being different, but I laugh at you for being the same. " +
-            "The consequences of today are determined by the actions of the past. To change your future, alter your decisions today. " +
-            "Experience is a hard teacher because she gives the test first, the lesson afterwards. " +
-            "Ability may get you to the top, but it takes character to keep you there. " +
-            "Life is not measured by the number of breaths we take, but by the moments that take our breath away." +
-            "I have a simple philosophy: Fill what's empty. Empty what's full. Scratch where it itches. " +
-            "Don't go around saying the world owes you a living. The world owes you nothing. It was here first. " +
-            "Being happy doesn't mean that everything is perfect. It means that you've decided to look beyond the imperfections. " +
-            "Do not pray for tasks equal to your powers.Pray for powers equal to your tasks.Then the doing of work shall be no miracle,but you shall be the miracle. " +
-            "Fear not that the life shall come to an end, but rather fear that it shall never have a beginning. " +
-            "At twenty years of age, the will reigns; at thirty, the wit; and at forty, the judgment. " +
-            "What gets us into trouble is not what we don't know.It's what we know for sure that just ain't so. " +
-            "Life is like a hot bath. It feels good while you’re in it, but the longer you stay in, the more wrinkled you get. " +
-            "Life is too short to wake up in the morning with regrets. So, love the people who treat you right and forget about the ones who do not. " +
-            "People who are serious about the relation are moody as they have devoted a lot that makes them worry about gains and losses. ",
-        chinese: "臣本布衣，躬耕南阳，苟全性命于乱世，不求闻达于诸侯。先帝不以\n" +
-            "臣卑鄙，猥自枉屈，三顾臣于草庐之中，谘臣以当世之事，由是感激，遂\n" +
-            "许先帝以驱驰。后值倾覆，受任于败军之际，奉命于危难之间：尔来二十\n" +
-            "有一年矣。先帝知臣谨慎，故临崩寄臣以大事也。受命以来，夙夜忧虑，\n" +
-            "恐付托不效，以伤先帝之明；故五月渡泸，深入不毛。今南方已定，甲兵\n" +
-            "已足，当奖帅三军，北定中原，庶竭驽钝，攘除奸凶，兴复汉室，还于旧\n" +
-            "都：此臣所以报先帝而忠陛下之职分也。至于斟酌损益，进尽忠言，则攸\n" +
-            "之、依、允等之任也。愿陛下托臣以讨贼兴复之效，不效则治臣之罪，以\n" +
-            "告先帝之灵；若无兴复之言，则责攸之、依、允等之咎，以彰其慢。陛下\n" +
-            "亦宜自谋，以谘诹善道，察纳雅言，深追先帝遗诏。臣不胜受恩感激！今\n" +
-            "当远离，临表涕泣，不知所云。",
+        "You laugh at me for being different, but I laugh at you for being the same. " +
+        "The consequences of today are determined by the actions of the past. To change your future, alter your decisions today. " +
+        "Experience is a hard teacher because she gives the test first, the lesson afterwards. " +
+        "Ability may get you to the top, but it takes character to keep you there. " +
+        "Life is not measured by the number of breaths we take, but by the moments that take our breath away." +
+        "I have a simple philosophy: Fill what's empty. Empty what's full. Scratch where it itches. " +
+        "Don't go around saying the world owes you a living. The world owes you nothing. It was here first. " +
+        "Being happy doesn't mean that everything is perfect. It means that you've decided to look beyond the imperfections. " +
+        "Do not pray for tasks equal to your powers.Pray for powers equal to your tasks.Then the doing of work shall be no miracle,but you shall be the miracle. " +
+        "Fear not that the life shall come to an end, but rather fear that it shall never have a beginning. " +
+        "At twenty years of age, the will reigns; at thirty, the wit; and at forty, the judgment. " +
+        "What gets us into trouble is not what we don't know.It's what we know for sure that just ain't so. " +
+        "Life is like a hot bath. It feels good while you’re in it, but the longer you stay in, the more wrinkled you get. " +
+        "Life is too short to wake up in the morning with regrets. So, love the people who treat you right and forget about the ones who do not. " +
+        "People who are serious about the relation are moody as they have devoted a lot that makes them worry about gains and losses. ",
+        chinese: `豫章故郡，洪都新府。星分翼轸，地接衡庐。襟三江而带五湖，控蛮荆而引瓯越。物华天宝，龙光射牛斗之墟；人杰地灵，徐孺下陈蕃之榻。雄州雾列，俊采星驰。台隍枕夷夏之交，宾主尽东南之美。都督阎公之雅望，棨戟遥临；宇文新州之懿范，襜帷暂驻。十旬休假，胜友如云，千里逢迎，高朋满座。腾蛟起凤，孟学士之词宗；紫电青霜，王将军之武库。家君作宰，路出名区；童子何知，躬逢胜饯！
+时维九月，序属三秋。潦水尽而寒潭清，烟光凝而暮山紫。俨骖騑于上路，访风景于崇阿。临帝子之长洲，得天人之旧馆。层台耸翠，上出重霄；飞阁翔丹，下临无地。鹤汀凫渚，穷岛屿之萦回，桂殿兰宫，列冈峦之体势。
+披绣闼，俯雕甍，山原旷其盈视，川泽纡其骇瞩。闾阎扑地，钟鸣鼎食之家；舸舰迷津，青雀黄龙之轴。云销雨霁，彩彻区明。落霞与孤鹜齐飞，秋水共长长天一色。渔舟唱晚，响穷彭蠡之滨，雁阵惊寒，声断衡阳之浦。
+  遥襟甫畅，逸兴遄飞。爽籁发而清风生，纤歌凝而白云遏。睢园绿竹，气凌彭泽之樽；邺水朱华，光照临川之笔。四美具，二难并。穷睇眄于中天，极娱游于暇日。天高地迥，觉宇宙之无穷；兴尽悲来，识盈虚之有数。望长安于日下，指吴会于云间。地势极而南溟深，天柱高而北辰远。关山难越，谁悲失路之人？萍水相逢，尽是他乡之客。怀帝阍而不见，奉宣室以何年？ 
+  嗟乎！时运不齐，命途多舛。冯唐易老，李广难封。屈贾谊于长沙，非无圣主；窜梁鸿于海曲，岂乏明时？所赖君子见几，达人知命。老当益壮，宁移白首之心？穷且益坚，不坠青云之志，酌贪泉而觉爽，处涸辙以犹欢。北海虽赊，扶摇可接；东隅已逝，桑榆非晚。孟尝高洁，空怀报国之情；阮籍猖狂，岂效穷途之哭？ 
+  勃，三尺微命，一介书生。无路请缨，等终军之弱冠；有怀投笔，爱宗悫之长风。舍簪笏于百龄，奉晨昏于万里。非谢家之宝树，接孟氏之芳邻。他日趋庭，叨陪鲁对；今兹捧袂，喜托龙门。杨意不逢，抚凌云而自惜；钟期既遇，奏流水以何惭？ 
+呜呼！胜地不常，盛筵难再。兰亭已矣，梓泽丘墟。临别赠言，幸承恩于伟饯；登高作赋，是所望于群公。敢竭鄙怀，恭疏短引，一言均赋，四韵俱成。请洒潘江，各倾陆海云尔。`,
     }
 })
